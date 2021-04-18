ad085babb0e96b73b7e4807ef3b9c5225be7dd80
ErikaD0203 <erikadulay12345678@gmail.com>
2021-04-18 20:37:13 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/capstone_dpa/Profile.java
@@ -0,0 +1,16 @@
+package mcm.edu.ph.capstone_dpa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
